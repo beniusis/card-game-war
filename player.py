e48f46a18d77baf57df855121292e563961d4615
beniusis <rokasbeniu@gmail.com>
2024-02-12 12:40:45 +0200

    chore: created player class

---
+++ player.py
@@ -0,0 +1,20 @@
+class Player:
+    def __init__(self, name, deck):
+        self.name = name
+        self.deck = deck
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def deck(self):
+        return self._deck
+
+    @deck.setter
+    def deck(self, deck):
+        self._deck = deck
