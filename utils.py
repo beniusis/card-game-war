4ece51bc13989a45219339986f34fbce7decce69
beniusis <rokasbeniu@gmail.com>
2024-02-12 12:23:46 +0200

    feat: before the game starts, user is asked to provide both names of the players

---
+++ utils.py
@@ -0,0 +1,4 @@
+def get_the_names_of_the_players():
+    player_one_name = input("Player's 1 name: ").strip()
+    player_two_name = input("Player's 2 name: ").strip()
+    return player_one_name, player_two_name
