2148ecd8d83d931a4dd9c5200a2fd73c32814607
beniusis <rokasbeniu@gmail.com>
2024-02-12 12:38:44 +0200

    feat: deck initialization with the 52 cards

---
+++ card.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Card:
+    rank: str
+    suit: str
+    value: int
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def main(): ...
+import utils
+
+
+def main():
+    cards = utils.initialize_deck()
+    for card in cards:
+        print(card)
 
 
 if __name__ == "__main__":
