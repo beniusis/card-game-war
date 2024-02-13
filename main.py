c1f90f65f30fd361059e4d26a0d0027d74a8740e
beniusis <rokasbeniu@gmail.com>
2024-02-13 15:31:10 +0200

    refactor: removed unnecessary lines of code

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ def main():
     name2 = utils.get_the_name_of_the_second_player()
     p1, p2 = utils.initialize_players(name1, name2)
 
-    while p1.deck_size and p2.deck_size:
+    round = 1
+    while p1.get_deck_size() and p2.get_deck_size():
+        print(f"Round: {round}")
         utils.play_round(p1, p2)
+        round += 1
 
-        if not p1.deck_size:
-            print(f'{p2.name} wins the game!')
-        elif not p2.deck_size:
-            print(f'{p1.name} wins the game!')
+        if not p1.get_deck_size():
+            print(f"{p2.name} wins the game!")
+        elif not p2.get_deck_size():
+            print(f"{p1.name} wins the game!")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -10,7 +10,9 @@ def get_the_name_of_the_first_player():
             raise ValueError
         return name
     except ValueError:
-        print(f"\n{TEXT_COLORS['error']}Player's name cannot be empty.\n{TEXT_COLORS['default']}")
+        print(
+            f"\n{TEXT_COLORS['error']}Player's name cannot be empty.\n{TEXT_COLORS['default']}"
+        )
         return get_the_name_of_the_first_player()
 
 
@@ -21,7 +23,9 @@ def get_the_name_of_the_second_player():
             raise ValueError
         return name
     except ValueError:
-        print(f"\n{TEXT_COLORS['error']}Player's name cannot be empty.\n{TEXT_COLORS['default']}")
+        print(
+            f"\n{TEXT_COLORS['error']}Player's name cannot be empty.\n{TEXT_COLORS['default']}"
+        )
         return get_the_name_of_the_second_player()
 
 
@@ -30,28 +34,23 @@ def initialize_players(name1, name2):
     cards = deck.cards
     return Player(name1, cards[::2]), Player(name2, cards[1::2])
 
+
 def draw_cards(p1, p2):
     return p1.draw_card(), p2.draw_card()
 
+
 def play_round(p1, p2):
     card1, card2 = draw_cards(p1, p2)
-
-    # print(f'{p1.name} deck: {p1.deck}, {p2.name} deck: {p2.deck}')
-    print(f'Compare cards: {card1.value}, {card2.value}')
-
     if card1.value > card2.value:
-        # print([card1,card2] + Player.pile)
-        print([card1,card2],Player.pile)
-        p1.add_cards([card1,card2] + Player.pile)
-        print(f'{p1.name} wins!')
+        p1.add_cards(Player.pile + [card1, card2])
+        Player.pile = []
+        print(f"{p1.name} wins!")
     elif card1.value < card2.value:
-        p2.add_cards([card1,card2] + Player.pile)
-        print(f'{p2.name} wins!')
+        p2.add_cards(Player.pile + [card1, card2])
+        Player.pile = []
+        print(f"{p2.name} wins!")
     else:
         Player.pile.extend([card1, card2] + list(draw_cards(p1, p2)))
-        print('Draw')
-        print(f'pile: {len(Player.pile)}')
+        print("Draw")
 
-    print(f'{p1.name} size: {p1.deck_size}, {p2.name} size: {p2.deck_size}')
-    # print(f'{p1.name} deck: {p1.deck}, {p2.name} deck: {p2.deck}')
-    print('-' * 30)
+    print("-" * 30)
