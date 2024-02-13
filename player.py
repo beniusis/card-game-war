b667770f2eb27a35d6a5d36783459b58d1e0108f
beniusis <rokasbeniu@gmail.com>
2024-02-13 15:31:35 +0200

    impr: before drawing the card, check if the length of the deck is not 0

--- player.py
+++ player.py
@@ -1,10 +1,12 @@
+import sys
+
+
 class Player:
     pile = []
 
     def __init__(self, name, deck):
         self.name = name
         self.deck = deck
-        self.deck_size = len(self.deck)
 
     @property
     def name(self):
@@ -23,7 +25,12 @@ class Player:
         self._deck = deck
 
     def draw_card(self):
+        if not self.deck:
+            sys.exit(f"No more cards left for {self.name}. Opponent wins!")
         return self.deck.pop(0)
 
     def add_cards(self, cards: list):
         self.deck.extend(cards)
+
+    def get_deck_size(self):
+        return len(self.deck)
