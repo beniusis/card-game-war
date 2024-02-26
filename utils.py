0857c2f0db379879240241214ad146d04b13b8c8
beniusis <rokasbeniu@gmail.com>
2024-02-26 19:33:47 +0200

    refactor: removed unnecessary variable assignment

--- utils.py
+++ utils.py
@@ -31,8 +31,7 @@ def get_the_name_of_the_second_player():
 
 def initialize_players(name1, name2):
     deck = Deck()
-    cards = deck.cards
-    return Player(name1, cards[::2]), Player(name2, cards[1::2])
+    return Player(name1, deck.cards[::2]), Player(name2, deck.cards[1::2])
 
 
 def draw_cards(p1, p2):
